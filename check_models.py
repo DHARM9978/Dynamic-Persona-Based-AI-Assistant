cc335e35202ee0bfd2d51d5590767b9f7639436a
DESKTOP-U8K6QCI\Admin <bhadanidharm9968@gmail.com>
2026-07-09 15:36:24 +0530

    Make a small chanmge and commit the file

--- check_models.py
+++ check_models.py
@@ -3,14 +3,13 @@ from google import genai
 import streamlit as st
 import os
 
-load_dotenv()
+# load_dotenv()
 
 # Use for the local run
 # client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Use for deployment
 client = genai.Client(api_key = st.secrets["GOOGLE_API_KEY"])
-
 
 for model in client.models.list():
     print(model.name)

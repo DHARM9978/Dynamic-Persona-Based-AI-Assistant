9a1d1f36347a0c6b26979c723ae23664bb44c149
DESKTOP-U8K6QCI\Admin <bhadanidharm9968@gmail.com>
2026-07-09 15:12:31 +0530

    Updating the remaining files in which api key code need to be changed

--- check_models.py
+++ check_models.py
@@ -1,10 +1,16 @@
 from google import genai
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
+import streamlit as st
 import os
 
 load_dotenv()
 
-client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+# Use for the local run
+# client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Use for deployment
+client = genai.Client(api_key = st.secrets["GOOGLE_API_KEY"])
+
 
 for model in client.models.list():
     print(model.name)
--- test_gemini.py
+++ test_gemini.py
@@ -1,12 +1,20 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
-load_dotenv()
+import streamlit as st
+
+# use for the local run
+# load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-flash",
-    google_api_key=os.getenv("GOOGLE_API_KEY")
+    
+    # Use for local run
+    # google_api_key=os.getenv("GOOGLE_API_KEY")
+
+    # Use for the deployment
+    google_api_key = st.secrets["GOOGLE_API_KEY"]
 )
 
 response = llm.invoke("Say hello")
